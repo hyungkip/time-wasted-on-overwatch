63d309a4d4b8ffaef0d60e44100fc93b8526dd21
Hyungki <hyungkip@gmail.com>
2016-08-17 11:24:30 -0700

    [feature] implements an input that successfully sends blizzard id to the external api call

--- client/main/mainController.js
+++ client/main/mainController.js
@@ -1,26 +1,27 @@
 angular.module('time-wasted-on-overwatch')
   .controller('mainController', ['$scope', '$http', function ($scope, $http) {
-    var searchID = function() {
-      return $http({
+    $scope.searchID = function (id) {
+      $http({
         method: 'GET',
-        url: 'https://api.lootbox.eu/pc/us/Hunky-1228/profile'
+        url: 'https://api.lootbox.eu/pc/us/' + id + '/profile'
       })
       .then(
         function (resp) {
+          console.log(resp);
           return resp.data;
-        }).catch(function (error) {
+        }).catch( function (error) {
           console.log(error);
         }
       );
     };
 
-    searchID().then(
-      function (resp) {
-        console.log(resp.data);
-        return resp.data;
-      }).catch(function (error) {
-        console.log(error);
-      });
+    // searchID().then(
+    //   function (resp) {
+    //     console.log(resp.data);
+    //     return resp.data;
+    //   }).catch( function (error) {
+    //     console.log(error);
+    //   });
 
 
   }]);
