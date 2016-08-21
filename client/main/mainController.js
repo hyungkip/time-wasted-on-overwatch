2d7419cc395bb460ea8ad2df67e6cecc43b85d4b
Hyungki <hyungkip@gmail.com>
2016-08-20 21:56:16 -0700

    [feature] adds formatting for IDs inputted with different kinds of separators (other than hyphen)

--- client/main/mainController.js
+++ client/main/mainController.js
@@ -3,13 +3,34 @@ angular.module('time-wasted-on-overwatch')
     $scope.daysPlayed, $scope.hoursPlayed, $scope.username, $scope.avatar, $scope.validationFailed, $scope.loading;
     $scope.inputEntered = false;
 
+    // $scope.searchID = function (id) {
+    //   $scope.validationFailed = false;
+    //   $scope.inputEntered = false;
+    //   $scope.loading = true;
+    //
+    //   searchLootBoxProfile(id).then( function (data) {
+    //     return retrieveInformation(data);
+    //   }).then(function (data) {
+    //     $scope.inputEntered = true;
+    //     $scope.validationFailed = false;
+    //     $scope.loading = false;
+    //     return;
+    //   }).catch( function (error) {
+    //     $scope.inputEntered = false;
+    //     $scope.validationFailed = true;
+    //     $scope.loading = false;
+    //     console.log(error);
+    //   });
+    // }
+
     $scope.searchID = function (id) {
       $scope.validationFailed = false;
       $scope.inputEntered = false;
       $scope.loading = true;
-      searchLootBoxProfile(id).then( function (data) {
-        return retrieveInformation(data);
-      }).then(function (data) {
+      var formattedID = formatID(id);
+      searchLootBoxProfile(formattedID)
+      .then( function (data) {
+        retrieveInformation(data);
         $scope.inputEntered = true;
         $scope.validationFailed = false;
         $scope.loading = false;
@@ -22,6 +43,30 @@ angular.module('time-wasted-on-overwatch')
       });
     }
 
+    var formatID = function (unformattedID) {
+    	//note: battlenet tag numbers will always be either 4 or 5 digits long
+    	//case if separator is a hyphen
+    	if (unformattedID[unformattedID.length - 5] == '-' || unformattedID[unformattedID.length - 6] == '-') {
+          	return unformattedID;
+      }
+
+      var possibleSeparators = ['#', '.', ' '];
+
+      //check for other separators
+    	for (var i=0; i<possibleSeparators.length; i++) {
+          if (unformattedID[unformattedID.length - 5] == possibleSeparators[i] || unformattedID[unformattedID.length - 6] == possibleSeparators[i]) {
+          	return unformattedID.split(possibleSeparators[i]).join('-');
+          }
+    	}
+
+    	//check for no separator
+    	for (var i=5; i>3; i--) {
+    		if (!isNaN(unformattedID[unformattedID.length - i])) {
+    			return unformattedID.substring(0, unformattedID.length - i) + '-' + unformattedID.substring(unformattedID.length - i, unformattedID.length)
+    		}
+    	}
+    	return unformattedID;
+    }
 
     var searchLootBoxProfile = function (blizzID) {
       return $http({
@@ -39,18 +84,30 @@ angular.module('time-wasted-on-overwatch')
 
     var retrieveInformation = function (obj) {
       //Format time played
-      var competitiveHoursPlayed = parseInt(obj.data.playtime.competitive.split(' ')[0]);
-      var quickHoursPlayed = parseInt(obj.data.playtime.quick.split(' ')[0]);
+      var competitiveHoursPlayed;
+      if (obj.data.playtime.competitive == undefined) {
+        competitiveHoursPlayed = 0;
+      }
+      else {
+        competitiveHoursPlayed = parseInt(obj.data.playtime.competitive.split(' ')[0]);
+      }
+
+      var quickHoursPlayed;
+      if (obj.data.playtime.quick == undefined) {
+        quickHoursPlayed = 0;
+      }
+      else {
+        quickHoursPlayed = parseInt(obj.data.playtime.quick.split(' ')[0]);
+      }
+
       var totalHoursPlayed = competitiveHoursPlayed + quickHoursPlayed;
       $scope.daysPlayed = Math.floor(totalHoursPlayed/24);
       $scope.hoursPlayed = totalHoursPlayed - $scope.daysPlayed*24;
       $scope.username = obj.data.username;
       $scope.avatar = obj.data.avatar;
-
-
     }
 
-
+    //Users able to press enter to search. Quick and dirty jQuery
     $(document).ready(function(){
       $('#textBox').keypress(function(e){
         if(e.keyCode==13)
